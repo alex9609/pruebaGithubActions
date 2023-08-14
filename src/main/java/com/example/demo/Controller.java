40f4fbac595bb40405783d4a80635329bdf3edc6
Alex <alrodriguez41@poligran.edu.co>
2023-08-14 07:32:56 -0500

    Cambio proyecto

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,15 +1,26 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/prueba")
 public class Controller {
 
+    Logger log = LoggerFactory.getLogger(Controller.class);
+
     @GetMapping
     public String helloworld(){
         return "<h1> Hello world </h1>";
+    }
+
+    @PostMapping(value = "/saludo")
+    @CrossOrigin
+    public ResponseEntity<String> helloworld(@RequestBody String mensaje){
+        String saludo  = mensaje;
+        log.info("El mensaje es : " + mensaje);
+        return ResponseEntity.ok("Mensaje asignado  " + saludo);
     }
 }
